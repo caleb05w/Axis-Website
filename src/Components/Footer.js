f051ee016ca52b3eeb4fba36c0c4fcd7a8d41038
caleb05w <99751567+caleb05w@users.noreply.github.com>
2024-10-04 02:12:22 -0700

    finished skeleton of home page

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,55 @@
 import React from 'react'
-
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
+import { MdOutlineEmail } from "react-icons/md";
 
 function Footer() {
 return ( 
-<div></div>
+<div>
+    <nav className='bg-[#151515] pt-[7%]'>
+        
+        <div className='flex justify-between w-[90%] m-auto'>
+            
+            <div className=''>
+                <h6 className='font-bold'> Axis Consulting </h6>
+                <div className='flex flex-col mt-[5%]'>
+                    <h6>8888 University Drive</h6>
+                    <h6>Burnaby, BC</h6>
+                    <h6>V5A 1S6</h6>
+                </div>
+            </div>
+        
+            <div className='flex w-[25%]  h-auto justify-between max-w-[100lvw]'>
+                <div className='flex flex-col gap-[0.5rem]'>
+                    <h6 className='font-bold'> Resources </h6>
+                    <a href='#'><h6>The Brand</h6></a>
+                    <a href='#'><h6>The Team</h6></a>
+                    <a href='#'><h6>Our Clients</h6></a>
+                    <a href='#'><h6>FAQ</h6></a>
+                </div>
+
+                <div className='flex flex-col gap-[0.5rem]'>
+                    <h6 className='font-bold'> Get Involved </h6>
+                    <a href='#'><h6>Contact Us</h6></a>
+                    <a href='#'><h6>Recruitment Package</h6></a>
+                    <a href='#'><h6>Apply Here</h6></a>
+                </div>
+            </div>
+
+        </div>
+
+        <div className='w-[90%] mx-auto mt-[3%] pb-[3%] flex justify-between'>
+            <h6 className='text-[#4998FF]'> Axis Consulting @ 2024-2025 </h6>
+            <div className='flex gap-[1%] w-[50%] justify-end'>
+                <a href='https://www.linkedin.com/company/18467218'><FaLinkedinIn color='white' /></a>
+                <a href='https://www.instagram.com/sfuaxisconsulting/'><FaInstagram color='white'/></a>
+                <a href='https://www.facebook.com/SFUAxisConsulting/'><FaFacebookF color='white'/></a>
+                <a href='google.com'><MdOutlineEmail color='white'/></a>
+            </div>
+        </div>
+    </nav>
+</div>
 )
 }
 
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,24 +1,92 @@
 import React from 'react'
 import '../output.css'
 import '../input.css' //<-- Tailwind stylesheet
-import gloriousPM from '../assets/images/gloriousPM.jpg'
-import justin from '../assets/images/justin.png'
-import justin2 from '../assets/images/justin2.png'
-import justin3 from '../assets/images/justin3.png'
-
+import { IoIosCheckmark } from "react-icons/io";
+import { MdOutlinePeopleAlt } from "react-icons/md";
+import { IoMdStarOutline } from "react-icons/io";
+import { BsPaperclip } from "react-icons/bs";
+import { FaRegHeart } from "react-icons/fa";
+import { FaRegStickyNote } from "react-icons/fa";
+import org from '../assets/images/org.png';
 
 function Home() {
   return (
     <div className='bg-[#151515]'> 
 
-      <div className=" w-[80%] m-auto py-[5%]">
+      <div className=" w-[70%] h-fit m-auto py-[5%] flex flex-col">
 
         <div className='grid justify-center text-center w-[50%] m-auto'>
         <h1 className=''> Personal Growth <span id='blue'> Community Impact </span></h1>
           <p className='w-[60%] mx-auto my-[5%]'>Axis Consulting is a student-led pro-bono consultancy part of Simon Fraser University.</p>
         </div>
 
+        <div className='grid grid-cols-2 gap-[0.8rem] mt-[5%]'>
+
+          <div className='grid-rows-3 grid gap-[0.8rem] min-w-fit max-h-lvh'>
+            <div className='flex gap-[8%] bg-[#1A1A1A] px-[5%] py-[3%] rounded-md'>
+            <IoIosCheckmark style={{color:'#4B7BFF'}} size='3lvw' />
+            <h3 className=''>63 Projects</h3>
+            </div>
+
+            <div className='flex gap-[8%] bg-[#1A1A1A] px-[5%] py-[3%] rounded-md'>
+            <MdOutlinePeopleAlt style={{color:'#4B7BFF'}} size='1.5lvw' />
+            <h3 className=''>60 Clients</h3>
+            </div>
+
+            <div className='flex gap-[8%] bg-[#1A1A1A] px-[5%] py-[3%] rounded-md'>
+            <IoMdStarOutline style={{color:'#4B7BFF'}} size='1.5lvw' />
+              <h3 className=''>500 Billable Hours/Project</h3>
+            </div>
+          </div>
+
+          <div className='grid-rows-3 grid gap-[0.8rem] min-w-fit max-h-lvh'>
+            <div className='flex gap-[8%] bg-[#1A1A1A] px-[5%] py-[3%] rounded-md'>
+            <BsPaperclip style={{color:'#4B7BFF'}} size='1.5lvw' />
+            <h3 className=''>212 Alumni</h3>
+            </div>
+
+            <div className='flex gap-[8%] bg-[#1A1A1A] px-[5%] py-[3%] rounded-md'>
+            <FaRegHeart style={{color:'#4B7BFF'}} size='1lvw' />
+            <h3 className=''>96% Client Satisfaction</h3>
+            </div>
+
+            <div className='flex gap-[8%] bg-[#1A1A1A] px-[5%] py-[3%] rounded-md'>
+            <FaRegStickyNote style={{color:'#4B7BFF'}} size='1.5lvw' />
+            <h3 className=''>92% Implementation Rate</h3>
+            </div>
+          </div>
+
+        </div>
+      
+      
+      <div className='w-[100%] m-auto text-center mt-[10%]'>
+        <h3> Our Engagements </h3>
+
+        <div className='grid grid-cols-2 h-[40rem] gap-[1%]'>
+            <div className='bg-white rounded-md w-[100%] mt-[5%]'>
+              <p>HJello</p>
+            </div>
+
+            <div className='bg-white rounded-md w-[100%] mt-[5%]'>
+              <p>HJello</p>
+            </div>
+        </div>
       </div>
+
+      <div className='relative w-[100%] h-fit rounded-md mt-[10%] text-center'>
+        <img className='brightness-[0.25]' src={org} alt='org' />
+
+          <div className='absolute top-[40%] w-[100%] m-auto'>
+            <div className='m-auto max-w-[33%] flex flex-col gap-[3lvh] '>
+              <h2> Ready to Soar?</h2>
+              <p> Join the team, come to events, and expand horizons within the field of consulting</p>
+                <div className='w-100% '>
+                  <button>  Get Involved </button>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
     </div>
 )
 }
